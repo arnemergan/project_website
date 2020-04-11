eb145c5881f2037d78506f125470b949a55f856f
arnemergan <mergan.arne@hotmail.com>
2020-04-11 20:53:59 +0200

    work3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    clientID: '90mwOOuTP4AV30DBvn4feK2B1ycoWvqV',
+    domain: 'dev-dildox8i.eu.auth0.com', // e.g., https://you.auth0.com/
+    audience: 'https://final-work-auth/', // e.g., http://localhost:3001
+    redirect: 'https://angular-frontend-final-work.herokuapp.com/callback',
+    scope: 'openid profile email'
+  }
 };
